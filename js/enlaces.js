03b83df9e6f9e701190a7a1da09dec0dae88a5c0
Susana Elizabeth Almaraz <sealmaraz@yahoo.com.ar>
2023-09-12 01:42:52 -0300

    se agrega arhivo index.css

--- js/enlaces.js
+++ js/enlaces.js
@@ -1,3 +1,6 @@
 document.getElementById('conocimiento').addEventListener('click',function(){
-    document.getElementById('listado_conoc').style.display='none';
+    document.getElementById('listado_conoc').style.display='block';
+});
+document.getElementById('trabajo').addEventListener('click',function(){
+    document.getElementById('listado_trabajo').style.display='block';
 });
